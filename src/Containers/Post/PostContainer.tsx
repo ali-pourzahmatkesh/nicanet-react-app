c61612b320813928bee9fc1a77970d69e9c0fa63
fatemehalimomeni <fateme.alimomeni@gmail.com>
2019-06-19 09:40:21 +0430

    delete post

--- src/Containers/Post/PostContainer.tsx
+++ src/Containers/Post/PostContainer.tsx
@@ -300,7 +300,7 @@ function PostContainer(
 
           {+user.PersonId === +WrittenById && (
             <DeleteBtn>
-              <Delete onClick={deleteConfirmation}>Delete Case</Delete>
+              <Delete onClick={deleteConfirmation}>Delete Post</Delete>
             </DeleteBtn>
           )}
 
